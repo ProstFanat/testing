2a0a26a5b59016cd8784dc11013bf9c61028cf28
Yastreba.Srhii <s.yastreba@gmail.com>
2020-11-17 18:07:46 +0200

    testtsts

--- src/main/java/Login/Login.java
+++ src/main/java/Login/Login.java
@@ -1,38 +1,17 @@
 package Login;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import resources.BasePage;
+import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.*;
-import resources.ConfProperties;
 
-public class Login extends BasePage {
-    public WebDriver driver;
+public class Login {
 
-    public Login(WebDriver driver) {
-        super(driver);
-    }
+    SelenideElement loginField = $x("//*[@id = 'email']");
+    SelenideElement loginBtn = $x("//input[@value='Log In']");
+    SelenideElement passwdField = $x("//*[@id = 'password']");
 
-    // найти поле логина
-    @FindBy(xpath = "//*[@id = 'email']")
-    private WebElement loginField;
-
-    // найти кнопку логин
-    @FindBy(xpath = "//input[@value='Log In']")
-    private WebElement loginBtn;
-
-    // найти поле для пароля
-    @FindBy(xpath = "//*[@id = 'password']")
-    private WebElement passwdField;
-
-    public void login(String login){
-        open("http://localhost:8080/TrackEnsure/login.do");
-        waitToVisibilityOf(10, loginField);
-        waitToBeClickable(10, loginField);
-        loginField.sendKeys(login);
+    public void login(){
+        loginField.sendKeys("10");
         passwdField.sendKeys("test");
         loginBtn.click();
-        sleep(500);
     }
 }
--- src/test/java/Main/Test111.java
+++ src/test/java/Main/Test111.java
@@ -2,55 +2,24 @@ package Main;
 
 import Login.Login;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
-import resources.BasePage;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-
-import java.util.concurrent.TimeUnit;
 
 public class Test111 {
-
-    public static WebDriver driver;
-    public static BasePage basePage;
     public static Login login;
 
     @BeforeClass
     public static void setup() {
         Configuration.timeout = 100000;
-
         open("http://localhost:8080/TrackEnsure/login.do");
-        driver = getWebDriver();
-        basePage = new BasePage(driver);
-        login = new Login(driver);
-
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-
-        login.login("10");
-    }
-
-    @Before
-    public void before(){
-
+        login = new Login();
+        Configuration.timeout = 6000;
     }
 
     @Test
     public void test(){
-        System.out.println("hello world");
+        login.login();
+
     }
 
-    @After
-    public void afterTest(){
-        Selenide.refresh();
-    }
-
-    @AfterClass
-    public static void tearDown() {
-        Selenide.closeWebDriver();
-    }
 }
