06f73a26b971e0392789bcb848b9b5d71cd631e8
Adrian <adrianboyshuk2001@gmail.com>
2020-11-18 12:33:46 +0200

    Добавил ветку dispatcherManager

---
+++ src/test/java/DispatcherManager/test/ListCreateDispatcher.java
@@ -0,0 +1,97 @@
+package DispatcherManager.test;
+
+import DispatcherManager.dispatcherList;
+import DispatcherManager.dispatcherMonitor;
+import DispatcherManager.openDispatcher;
+import fleetEvents.fleet.OpenFleetPage;
+import fleetEvents.fleet.createNewFleetEvent;
+import fleetEvents.fleet.mainFleet;
+import fleetEvents.fleet.resources.ConfPropertiesFleet;
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import resources.BasePage;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class ListCreateDispatcher {
+
+    public static WebDriver driver;
+    public static openDispatcher open;
+    public static createNewFleetEvent createTrip;
+    public static fleetEvents.fleet.mainFleet mainFleet;
+    public static BasePage basePage;
+    public static dispatcherList main;
+    public static dispatcherMonitor mainMonitor;
+    public static OpenFleetPage openFleet;
+
+
+
+    @BeforeClass
+    public static void setup() {
+//        //определение пути до драйвера и его настройка
+//        System.setProperty("webdriver.chrome.driver", ConfPropertiesFleet.getProperty("chromedriver"));
+//        //создание экземпляра драйвера
+//        driver = new ChromeDriver();
+        open("http://localhost:8080/TrackEnsure/login.do");
+        driver = getWebDriver();
+        open = new openDispatcher(driver);
+        createTrip = new createNewFleetEvent(driver);
+        mainFleet = new mainFleet(driver);
+        basePage = new BasePage(driver);
+        driver.manage().window().maximize();
+        mainMonitor = new dispatcherMonitor(driver);
+        main= new dispatcherList(driver);
+        openFleet = new OpenFleetPage(driver);
+        open = new openDispatcher(driver);
+        //задержка на выполнение теста = 10 сек.
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        //получение ссылки на страницу входа из файла настроек
+        driver.get("http://localhost:8080/TrackEnsure/login.do");
+        open.openDispatchers();
+        basePage.sleep(1000);
+    }
+
+    @Before
+    public void beforeTest(){
+    }
+
+    @Test
+    public void testCreateDispatcher(){
+        int number = (int) (Math.random() * 1000000);
+        main.createNewDispatcher("testAdrian" + number);
+        main.inputSearchDispatcher("testAdrian" + number);
+        Assert.assertEquals(main.name.getText(), "testAdrian" + number);
+    }
+
+    @Test
+    public void testCreateDispatcherWithAlreadyExistsName(){
+        int number = (int) (Math.random() * 1000000);
+        main.createNewDispatcher("testAdrian" + number);
+        main.createNewDispatcher("testAdrian" + number);
+        if(main.isElementDisplayedByPath("//*[text() = 'Dispatcher " + "testAdrian" + number + " already exists']")){
+            main.inputSearchDispatcher("testAdrian" + number);
+            Assert.assertEquals(1, main.table.size());
+        }
+    }
+
+    @Test
+    public void testWithEmpty(){
+        main.openFormAddNewDispatcher();
+        Assert.assertFalse(main.btnSave.isEnabled());
+    }
+
+
+    @After
+    public void afterTests(){
+        driver.navigate().refresh();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/fleetEvents/fleet/OpenFleetPage.java
@@ -0,0 +1,64 @@
+package fleetEvents.fleet;
+
+import resources.BasePage;
+import resources.ConfProperties;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class OpenFleetPage extends BasePage {
+
+    // найти поле логина
+    @FindBy(xpath = "//*[@id = 'email']")
+    private WebElement loginField;
+
+    // найти кнопку логин
+    @FindBy(xpath = "//input[@value='Log In']")
+    private WebElement loginBtn;
+
+    // найти поле для пароля
+    @FindBy(xpath = "//*[@id = 'password']")
+    private WebElement passwdField;
+
+    // найти иконку fleet Events
+    @FindBy(xpath = "//div[@id = 'sidebar']//a[16]")
+    private WebElement fleet;
+
+    public OpenFleetPage(WebDriver driver) {
+        super(driver);
+    }
+
+    // открыть страницу
+    public void openFleetEvents(){
+        waitToVisibilityOf(10, loginField);
+        waitToBeClickable(10, loginField);
+        loginField.sendKeys(ConfProperties.getProperty("fleet.login"));
+        passwdField.sendKeys(ConfProperties.getProperty("fleet.password"));
+        loginBtn.click();
+        sleep(500);
+        driver.get("http://localhost:8080/TrackEnsure/app/fleet-events/#/trip-view(details:trip-view-details)");
+    }
+
+    /** Найти поле для трип номера*/
+    @FindBy(xpath = "//*[@placeholder = 'Search Trip Number']//input")
+    private WebElement inputTripNumber;
+
+    /** Найти кнопку поиска для трип номера*/
+    @FindBy(xpath = "//*[@placeholder = 'Search Trip Number']//..//button")
+    private WebElement btnSearchTrip;
+
+    /** Найти трип в списке*/
+    @FindBy(xpath = "//*[@placeholder = 'Search Trip Number']//ng-dropdown-panel")
+    private WebElement tripFromList;
+
+    public void findTrip(String trip){
+        waitToVisibilityOf(10, inputTripNumber);
+        waitToBeClickable(10, inputTripNumber);
+        inputTripNumber.sendKeys(trip);
+        btnSearchTrip.click();
+        waitToBeClickable(10, tripFromList);
+        sleep(500);
+        tripFromList.click();
+    }
+
+}
