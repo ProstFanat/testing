65f3702df8f39e08601d5f7261810245fab115bf
AlexandrBerkunsky <berkyt9@gmail.com>
2021-02-03 17:56:00 +0200

    some fixes

--- src/main/java/Migration/DAO/AclUserInGroupDAO.java
+++ src/main/java/Migration/DAO/AclUserInGroupDAO.java
@@ -26,7 +26,10 @@ public class AclUserInGroupDAO {
         ArrayList<String> aclUsersInGroup = new ArrayList<>();
 
         Connection connection = DBConnection.getConnection(db, user, pass);
-        String sql = "SELECT * from public.acl_user_in_group WHERE user_id IN (SELECT user_id from public.acl_user WHERE organization_id = " + orgId + ") ORDER BY group_id";
+        String sql = "SELECT DISTINCT auig.*, aug.*, aug.user_group AS group_name FROM acl_user_in_group auig "
+                + "JOIN acl_user_group aug ON aug.group_id = auig.group_id WHERE "
+                + "auig.user_id IN (SELECT user_id from public.acl_user WHERE organization_id = " + orgId + ") ORDER BY group_name";
+//        String sql = "SELECT * from public.acl_user_in_group WHERE user_id IN (SELECT user_id from public.acl_user WHERE organization_id = " + orgId + ") ORDER BY group_id";
         try (PreparedStatement ps = connection.prepareStatement(sql)){
             ResultSet rs = ps.executeQuery();
             while (rs.next()){
